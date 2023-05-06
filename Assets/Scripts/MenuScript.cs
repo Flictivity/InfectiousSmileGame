1554d80c944c91c8d6be483039f0a23114a8f9ad
Flictivity <bulat_sa1@mail.ru>
2023-05-06 14:36:39 +0300

    added jumping cubes in main menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,11 +1,11 @@
 using Assets.Scripts;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    [SerializeField] private GameObject[] Cubes;
     public void SelectPlayersCount(int count)
     {
         DataHolder.PlayerCount = count;
@@ -15,5 +15,23 @@ public class MenuScript : MonoBehaviour
     {
         DataHolder.Level = level;
         SceneManager.LoadScene("SampleScene");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AnimateCubes());
+    }
+
+    private IEnumerator AnimateCubes()
+    {
+        while (true)
+        {
+            foreach (var cub in Cubes)
+            {
+                var body = cub.GetComponent<Rigidbody2D>();
+                body.AddForce(new Vector2(0, Random.Range(600f, 1000f)));
+            }
+            yield return new WaitForSeconds(2);
+        }
     }
 }
